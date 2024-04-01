d354c08a8ff9ac2ffa60df6e5f2466d427bf20f7
mohamedameen01 <ameentp64@gmail.com>
2024-04-01 23:34:14 +0530

    Some Changes on Projects

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
-import Weather from '../assets/weather-app.jpg'
-import TaskManager from '../assets/task-manager.png'
-import Registration from '../assets/registration.jpg'
+import Ecommerce from "../assets/ecommerce.jpg";
+import SocialMedia from "../assets/social-media.png";
 
 function Projects() {
   return (
-    <section id='projects' className='text-white mx-auto max-w-[1280px] p-3'>
-        <h1 className='text-center md:text-start capitalize text-xl md:text-2xl m-4'>My Works</h1>
-        <div className="grid md:grid-cols-3 xl:grid-cols-3">
-            <div className="md:w-[240px] lg:w-[300px] xl:w-[400px] p-4 box-border ">
-                <a href="https://myweatherrepo.netlify.app/" rel="noreferrer" target='_blank'>
-                    <img className='w-[400px] h-[200px] rounded-xl cursor-pointer shadow-lg shadow-gray-400 hover:scale-105' src={Weather} alt="" />
-                    <h1 className='text-center capitalize lg:text-xl m-2'>Weather App</h1>    
-                </a>
-            </div>
-            <div className="md:w-[240px] lg:w-[300px] xl:w-[400px] p-4 box-border ">
-                <a href="https://remindmetask.netlify.app/" rel="noreferrer" target='_blank'>
-                    <img className='w-[400px] h-[200px] rounded-xl cursor-pointer shadow-lg shadow-gray-400 hover:scale-105' src={TaskManager} alt="" />
-                    <h1 className='text-center capitalize lg:text-xl m-2'>Task Manager</h1>    
-                </a>
-            </div>
-            <div className="md:w-[240px] lg:w-[300px] xl:w-[400px] p-4 box-border ">
-                <a href='https://3d-registration.vercel.app/' rel='noreferrer' target='_blank'>
-                    <img className='w-[400px] h-[200px] rounded-xl cursor-pointer shadow-lg shadow-gray-400 hover:scale-105' src={Registration} alt="" />
-                    <h1 className='text-center capitalize lg:text-xl m-2'>3D Registration</h1>    
-                </a>
-            </div>
+    <section id="projects" className="text-white mx-auto max-w-[1280px] p-3">
+      <h1 className="text-center font-semibold md:text-start capitalize text-xl md:text-2xl m-4 ">
+        My Works
+      </h1>
+      <div className="grid md:grid-cols-3 xl:grid-cols-3">
+        <div className="md:w-[240px] lg:w-[300px] xl:w-[400px] p-4 box-border ">
+          <a
+            href="https://mern-memories24.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              className="w-[400px] h-[200px] rounded-xl cursor-pointer shadow-lg shadow-gray-400 hover:scale-105"
+              src={SocialMedia}
+              alt="social-media-app"
+            />
+            <h1 className="text-center capitalize lg:text-xl m-2 mt-4">
+              SocialMedia App
+            </h1>
+          </a>
         </div>
+        <div className="md:w-[240px] lg:w-[300px] xl:w-[400px] p-4 box-border ">
+          <a
+            href="https://mern-ecommerce-cart-app.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              className="w-[400px] h-[200px] rounded-xl cursor-pointer shadow-lg shadow-gray-400 hover:scale-105"
+              src={Ecommerce}
+              alt="ecommerce-app"
+            />
+            <h1 className="text-center capitalize lg:text-xl m-2 mt-4">
+              Ecommerce App
+            </h1>
+          </a>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
